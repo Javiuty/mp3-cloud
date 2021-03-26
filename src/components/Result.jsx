e79e47d6cdd22d43fd525177c40f62a36a62f60c
Javiuty <javiiescuadra@gmail.com>
2021-03-26 22:51:41 +0100

    more style in songs

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,4 +1,4 @@
-import { shortString } from "../helpers";
+import { shortString, separateTags } from "../helpers";
 
 const Result = ({ cancion }) => {
   const {
@@ -15,13 +15,14 @@ const Result = ({ cancion }) => {
   } = cancion;
 
   return (
-    <article className="flex border-b w-full">
+    <article className="flex border-b w-full relative">
       <div>
         <img className="w-44 h-auto p-4" src={image} alt={title} />
       </div>
+
       <div className="flex flex-col justify-center pr-4">
         <svg
-          className="w-6 h-6 mb-8 cursor-pointer"
+          className="w-6 h-6 mb-8 cursor-not-allowed"
           fill="none"
           stroke="currentColor"
           viewBox="0 0 24 24"
@@ -35,52 +36,102 @@ const Result = ({ cancion }) => {
           ></path>
         </svg>
         <svg
-          class="w-6 h-6 cursor-pointer"
+          className="w-6 h-6 cursor-pointer"
           fill="none"
           stroke="currentColor"
           viewBox="0 0 24 24"
           xmlns="http://www.w3.org/2000/svg"
         >
           <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
             d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M9 19l3 3m0 0l3-3m-3 3V10"
           ></path>
         </svg>
       </div>
+
       <div>
         <div className="pt-4">
           <h3 className="font-bold">{title}</h3>
-          <h4>{shortString(description)}</h4>
+          <h4 className="text-sm">
+            {shortString(description) || "No hay descripción para este vídeo"}
+          </h4>
         </div>
-        <div className="pt-4">
-          <p className="italic">{tags}</p>
+        <div className="my-1">
+          <p className="text-sm">
+            #tags:
+            <span className="italic ml-1">{separateTags(tags)}</span>
+          </p>
+        </div>
+        <div>
+          <ul className="flex">
+            <li className="flex">
+              <svg
+                className="w-6 h-6"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                ></path>
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
+                ></path>
+              </svg>
+              {views}
+            </li>
+            <li className="flex">
+              <svg
+                className="w-6 h-6"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M17.657 18.657A8 8 0 016.343 7.343S7 9 9 10c0-2 .5-5 2.986-7C14 5 16.09 5.777 17.656 7.343A7.975 7.975 0 0120 13a7.975 7.975 0 01-2.343 5.657z"
+                ></path>
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M9.879 16.121A3 3 0 1012.015 11L11 14H9c0 .768.293 1.536.879 2.121z"
+                ></path>
+              </svg>
+              {favoritos}
+            </li>
+          </ul>
         </div>
       </div>
-      {/*
-  
-        <div>
-          <h2 className="font-bold">{title}</h2>
-          <p>{description ? description : "No hay descripción del vídeo"}</p>
-        </div>
-        <div className="flex items-center">
-          <svg
-            className="w-6 h-6 cursor-pointer"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z"
-            ></path>
-          </svg>
-        </div>
-      </div> */}
+
+      <div className="absolute centered-end border py-1 cursor-not-allowed">
+        <svg
+          className="w-6 h-6"
+          fill="none"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z"
+          ></path>
+        </svg>
+      </div>
     </article>
   );
 };
--- src/helpers.js
+++ src/helpers.js
@@ -1,7 +1,11 @@
 export function shortString(string) {
-  if (string.length > 95) {
-    return string.slice(0, 95) + "...";
+  if (string.length > 100) {
+    return string.slice(0, 100) + "...";
   } else {
     return string;
   }
+}
+
+export function separateTags(tags) {
+  console.log(tags);
 }
